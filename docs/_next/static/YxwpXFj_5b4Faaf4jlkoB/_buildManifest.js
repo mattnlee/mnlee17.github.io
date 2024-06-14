d597f4ba84653d4bfa7e3354ba3374a37f2ebb3f
Matthew Lee <mnlee17@users.noreply.github.com>
2024-06-14 15:00:01 -0700

    Deploy 9618293d765405e2e2a5cccc55c7f886b4cadfee

---
+++ docs/_next/static/YxwpXFj_5b4Faaf4jlkoB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-5297fb3f0ed781dc.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
