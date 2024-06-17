af7ccdf5c3634870c7b6c61632389a829ad31b87
Matthew Lee <mnlee17@users.noreply.github.com>
2024-06-17 16:48:43 -0700

    successful tic-tac-toe game imported into "Projects" section of site

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,18 +3,16 @@ export function Header() {
     <div style={{ border: "3px solid red" }}>
       <ul>
         <li>
-          <a class="active" href="/">
-            Home
-          </a>
+          <a href="/">Home</a>
         </li>
         <li>
-          <a href="/projects">PB&J Projects</a>
+          <a href="/projects">Projects</a>
         </li>
         <li>
           <a href="#contact">Contact</a>
         </li>
         <li>
-          <a href="#about">About</a>
+          <a href="/about">About</a>
         </li>
       </ul>
     </div>
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Header } from "../components/Header";
-import { Game } from "../components/game/TicTacToe";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Projects() {
   return (
     <main>
       <Header />
-      <h1>My Projects</h1>
-      <li>A Tic-tac-toe game:</li>
-      <Game />
+      <h1>About Page</h1>
+      <p>Hi there! My name is Matthew Lee. I coded this webpage.</p>
     </main>
   );
 }
